3691806d122b323caceaa002cb613354bb784f62
Chintan Radia <radiachints1512@gmail.com>
2015-08-26 12:19:44 +0530

    xo error resolved

--- cli.js
+++ cli.js
@@ -2,17 +2,17 @@ var ipInfo = require('ipinfo');
 var chalk = require('chalk');
 var Table = require('cli-table');
 var tbl = new Table({
-    head: [chalk.magenta.bold('Title'), chalk.magenta.bold('Value')]
+	head: [chalk.magenta.bold('Title'), chalk.magenta.bold('Value')]
 });
 
 ipInfo(function (err, currIpInfo) {
-    if (err) {
-        return;
-    }
-    for (var key in currIpInfo) {
-        if (currIpInfo.hasOwnProperty(key)) {
-            tbl.push([chalk.cyan(key), chalk.green(currIpInfo[key])]);
-        }
-    }
-    console.log(tbl.toString());
+	if (err) {
+		return;
+	}
+	for (var key in currIpInfo) {
+		if (currIpInfo.hasOwnProperty(key)) {
+			tbl.push([chalk.cyan(key), chalk.green(currIpInfo[key])]);
+		}
+	}
+	console.log(tbl.toString());
 });
